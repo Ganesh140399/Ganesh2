8a2f5e6e3d7b9cacf52a28105f2f245abf644c3a
Lenovo <Lenovo@LAPTOP-91EJ1UJ3>
2022-10-27 12:52:59 +0530

    Commit1

---
+++ Ganesh_Patil/src/ganu/Test1.java
@@ -0,0 +1,14 @@
+package ganu;
+
+public class Test1 
+{
+  public static void main(String[] args)
+  {
+	
+	  System.out.println("My name is Ganesh Patil");
+	  System.out.println("I have performed Automation Testing");
+	   
+  }
+	
+	
+}
